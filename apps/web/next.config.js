749a1173202d43915acf435d6981b84882e9e241
krishna <gohilkrishna9004@gmail.com>
2025-02-11 19:56:19 +0530

    q

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  transpilePackages: ["@repo/db"], 
   images: {
     remotePatterns: [
       {
@@ -21,8 +23,6 @@ const nextConfig = {
       }
     ],
   },
-}
+};
 
-
-export default nextConfig
-
+export default nextConfig;
